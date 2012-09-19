f23139f1b01ad05e3e11e758c425cddf72473b3a
Shaun Carlson <shaun.carlson@gmail.com>
2012-09-19 05:11:38 -0600

    Finished project

---
+++ project-2/cat1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+int outputToStdout(FILE*, int);
+
+int main(int argc, char* argv[]) {
+	int printLineNumbers = 0;
+	if (argc == 1) outputToStdout(stdin, printLineNumbers); //when no params passed in
+	int argCounter = 1;
+	if (argc > 1) {
+		if (strcmp(argv[1], "-n") == 0) {
+			//consume param and set line number flag
+			printLineNumbers = 1;
+			argCounter++;
+			if (argc == 2) outputToStdout(stdin, printLineNumbers); //when only -n passed in
+		} else if (strncmp(argv[1], "-", 1) == 0 && strlen(argv[1]) > 1) {
+			//checks if an invalid option was passed in
+			perror("usage: ./cat1 [-n] [file ...]");
+			return 1;
+		}
+	}
+
+	while (argCounter < argc) {
+		FILE* file;
+		if (strcmp(argv[argCounter], "-") == 0) {
+			file = stdin;
+		} else {
+			file = fopen(argv[argCounter], "r");
+			if (file == NULL) {
+				perror("No such file found");
+				continue;
+			}
+		}
+
+		outputToStdout(file, printLineNumbers);
+		fclose(file);
+		argCounter++;
+	}
+
+	return 0;
+}
+
+int outputToStdout(FILE* file, int printLineNumbers) {
+	char currentChar;
+	char prevChar = '\n';
+	int lineCounter = 1;
+	while ((currentChar = getc(file)) != EOF) {
+		if (printLineNumbers && prevChar == '\n') printf("%6d\t", lineCounter++);
+		putchar(currentChar);
+		prevChar = currentChar;
+	}
+
+	if (ferror(file)) {
+		perror("There was an error reading from file");
+		return 1;
+	}
+
+	return 0;
+}
